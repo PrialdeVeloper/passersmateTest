9b2b9a302dbc775d8bd9efffd3f0abd8584b979b
PrialdeVeloper <Prialde01@gmail.com>
2018-06-03 19:57:16 +0800

    undone approve

--- seeker/dashboard.php
+++ seeker/dashboard.php
@@ -6,12 +6,12 @@ $contact = '126-2634-252';
 $jtitle = "Asawa";
 $sd = "Nov 6, 2017";
 $ed = "Infinty";
-$reviewer = "Syrel Prialde";
+$reviewer = "Pablo";
 $rate = "Very Good!";
 ?>
 <?php 
 	require "../header-footer/seekerHeader.marvee";
-	require "modal/dashboard/seekerModal.html";
+	require "modal/dashboardModal.html";
 ?>
 <!-- jobs -->
 <div class="mt-4 d-flex justify-content-center">
@@ -132,11 +132,11 @@ $rate = "Very Good!";
 			</div>
 			<div class="row pt-2 hoverAccountSeeker">
 				<div class="col-sm-1 ml-3"><i class="fas fa-comments text-white"></i></div>
-				<div class="col-md-auto"><label class="text-white">Messages</label></div>
+				<div class="col-md-auto"><label><a href="message.php" class="text-white">Messages</a></label></div>
 			</div>
 			<div class="row pt-2 hoverAccountSeeker">
 				<div class="col-sm-1 ml-3"><i class="fas fa-people-carry text-white"></i></div>
-				<div class="col-md-auto"><label class="text-white">My Choosen Partners</label></div>
+				<div class="col-md-auto"><label><a href="messages.php" class="text-white">My Choosen Partners</a></label></div>
 			</div>
 			<div class="row pt-2 hoverAccountSeeker">
 				<div class="col-sm-1 ml-3"><i class="fas fa-star text-white"></i></div>
@@ -147,7 +147,10 @@ $rate = "Very Good!";
 			</div>
 			<div class="row pt-2 hoverAccountSeeker">
 				<div class="col-sm-1 ml-3"><i class="fas fa-handshake text-white"></i></div>
-				<div class="col-md-auto"><label class="text-white">My Employement Agreement Records</label></div>
+				<div class="col-md-auto">
+					<label class="text-white" id="smaller">My Agreement Records
+					</label>
+				</div>
 			</div>
 		</div>
 		<!-- end of 1st box 2nd row -->
@@ -199,92 +202,63 @@ $rate = "Very Good!";
 		<!-- end of 1st box 3rd row -->
 		<!-- start of 2nd box 3rd row -->
 	<div class="col offset-sm-1 border rounded bg-white mx-5 shadowDiv">
-			<div class="container mt-4 text-center">
-				<label id="ashGray" class="lead text-center">
-				Your Action awaits, Mate!
-				</label>
-      		</div>
+		<div class="container mt-4 text-center">
+			<label id="ashGray" class="lead text-center">
+			Your Action awaits, Mate!
+			</label>
+  		</div>
 
-      		<div class="d-flex justify-content-center">
-      			<div class="row pt-1 ">
-					<div class="col-sm-auto">
-						<label">Pending Actions</label>
-					</div>
+  		<div class="d-flex justify-content-center">
+  			<div class="row pt-1 ">
+				<div class="col-sm-auto">
+					<label">Pending Actions</label>
 				</div>
-      		</div>
+			</div>
+  		</div>
 
-	      	<div class="row mt-1">
-	      		<table class="table table-striped">
-	      			<thead class="skyblue text-white">
-	      				<tr>
-	      					<th>Image</th>
-	      					<th>Passers Name</th>
-	      					<th>Job Title</th>
-	      					<th>Start Date</th>
-	      					<th>End Date</th>
-	      					<th>Action</th>
-	      				</tr>
-	      			</thead>
-	      			<tbody>
-	      				<tr>
-	      					<td class="pt-5">
-	      						<img class="img-fluid tableImage" src="../etc/images/user/marvee.jpg">
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$Name;?>
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$jtitle;?>
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$sd;?>
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$ed;?>
-	      					</td>
-	      					<td>
-	      						<div class="container-fluid">
-	      							<div class="row">
-	      								<button class="btn btn-outline-success">Job Done</button>
-	      							</div>
-	      							<div class="row">
-	      								<button class="btn btn-outline-danger">Job Done</button>
-	      							</div>
-	      						</div>
-	      					</td>
-	      				</tr>
-	      				<tr>
-	      					<td class="pt-5">
-	      						<img class="img-fluid tableImage" src="../etc/images/user/marvee.jpg">
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$Name;?>
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$jtitle;?>
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$sd;?>
-	      					</td>
-	      					<td class="pt-5">
-	      						<?=$ed;?>
-	      					</td>
-	      					<td>
-	      						<div class="container-fluid">
-	      							<div class="row">
-	      								<button class="btn btn-outline-success">Job Done</button>
-	      							</div>
-	      							<div class="row">
-	      								<button class="btn btn-outline-danger">Job Done</button>
-	      							</div>
-	      						</div>
-	      					</td>
-	      				</tr>
-	      			</tbody>
-	      		</table>
-	      	</div>
-		</div>
-		<!-- end of 2nd box 3rd row -->
+      	<div class="table-responsive-xl row mt-1">
+      		<table class="table table-striped">
+      			<thead class="skyblue text-white">
+      				<tr>
+      					<th>Image</th>
+      					<th>Passers Name</th>
+      					<th>Job Title</th>
+      					<th>Start Date</th>
+      					<th>End Date</th>
+      					<th>Action</th>
+      				</tr>
+      			</thead>
+      			<tbody>
+      				<tr>
+      					<td class="pt-5">
+      						<img class="img-fluid tableImage" src="../etc/images/user/marvee.jpg">
+      					</td>
+      					<td class="pt-5">
+      						<?=$Name;?>
+      					</td>
+      					<td class="pt-5">
+      						<?=$jtitle;?>
+      					</td>
+      					<td class="pt-5">
+      						<?=$sd;?>
+      					</td>
+      					<td class="pt-5">
+      						<?=$ed;?>
+      					</td>
+      					<td>
+  							<div class="row">
+  								<button class="btn btn-outline-success" data-toggle="modal" data-target="#jobDone">Job Done</button>
+  							</div>
+  							<div class="row pt-1">
+  								<button class="btn btn-outline-danger">Report</button>
+  							</div>
+      					</td>
+      				</tr>
+      			</tbody>
+      		</table>
+      	</div>
+	</div>
+	<!-- end of 2nd box 3rd row -->
 	</div>
 	<!-- end of 3rd row -->
 </div>
